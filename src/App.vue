a73010f8aea46573a4b41867c86228f291f710bd
clumsy <z939701248@gmail.com>
2024-05-07 22:19:45 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="box">myApp</div>
+</template>
+
+<script setup lang="ts">
+const cc = 'woaininzuguo'
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 10px;
+  height: 20px;
+  background-color: aqua;
+}
+</style>
